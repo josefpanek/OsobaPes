6dbf203002f38efdf05de5ce0b2df1c17fbff976
Pepa Pánek <josef.panek@gmail.com>
2022-05-19 15:05:15 +0200

    drobné úpravy

--- OsobaPes/Program.cs
+++ OsobaPes/Program.cs
@@ -10,15 +10,20 @@ namespace OsobaPes
     {
         static void Main(string[] args)
         {
+            // azor je instance třídy Pes.
             Pes azor = new Pes("Azor", 1);
 
+            // lenka a karel jsou instance třídy Osoba.
             Osoba lenka = new Osoba("Lenka", azor);
             Osoba karel = new Osoba("Karel", azor);
 
+            // Vypíše jméno psa a jeho počáteční věk.
             Console.WriteLine("{0} ({1} let)", azor, azor.VypisVek());
+            // Nechám psa dvakrát zestárnout.
             azor.Zestarni();
             azor.Zestarni();
-            Console.WriteLine("{0} ({1} let)", lenka.pes, azor.VypisVek());
+            // Vypíše jméno Lenčina psa a jeho aktuální věk.
+            Console.WriteLine("{0} ({1} let)", lenka.pes, lenka.pes.VypisVek());
 
             Console.ReadKey();
         }
